5344b0f8b07c086996ddcb4ba868cfc96dbc4f30
Gajraj singh bhati <gsbgajraj@gmail.com>
2024-08-28 22:54:23 +0530

    first commit

---
+++ node JS/index.js
@@ -0,0 +1,3 @@
+const bhati=require("./second");
+console.log("Gajraj Singh is frontand backend developer")
+console.log(bhati)
---
+++ node JS/second.js
@@ -0,0 +1,6 @@
+bhati={
+    name: "Gajraj bhati",
+    age: 21,
+    student:true
+}
+module.exports= bhati
